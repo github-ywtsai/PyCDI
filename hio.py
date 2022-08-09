cd7cf08f4668dd537549bc39732a4c2df5d0e62c
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-08-09 17:24:52 +0800

    change fft&ifft to fft2&ifft2

--- hio.py
+++ hio.py
@@ -17,11 +17,11 @@ def HIO(last_Rspace, support, beta1, beta2, measured_amplitude, patched_ROI):
     support_region[support_region<0] = support_region[support_region<0] - beta2*support_region[support_region<0]
     last_Rspace[support] = support_region #loose support內不能有電子密度<0
 
-    F = np.fft.fftshift(np.fft.fft(np.fft.ifftshift(last_Rspace))) #將上次的結果fft得到新的相位猜測
+    F = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(last_Rspace))) #將上次的結果fft得到新的相位猜測
     phase = np.angle(F) #取出F的相位資訊
     F[patched_ROI] = np.multiply(np.exp(1j*phase[patched_ROI]),measured_A[patched_ROI]) #將ROI區的相位套上measured amplitude，其餘地方放著不動
     A = np.absolute(F)
-    R = np.real(np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(F)))) #將F1做ifft得到新的real space
+    R = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F)))) #將F1做ifft得到新的real space
 
     diff_err = DiffErr.DiffErr(A,measured_A) #計算diffraction error
 
