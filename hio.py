7248c9eca022d01637f64911836c287798feb086
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-08-09 14:00:43 +0800

    change the timimg to get amplitude of F

--- hio.py
+++ hio.py
@@ -18,9 +18,9 @@ def HIO(last_Rspace, support, beta1, beta2, measured_amplitude, patched_ROI):
     last_Rspace[support] = support_region #loose support內不能有電子密度<0
 
     F = np.fft.fftshift(np.fft.fft(np.fft.ifftshift(last_Rspace))) #將上次的結果fft得到新的相位猜測
-    A = np.absolute(F)
     phase = np.angle(F) #取出F的相位資訊
     F[patched_ROI] = np.multiply(np.exp(1j*phase[patched_ROI]),measured_A[patched_ROI]) #將ROI區的相位套上measured amplitude，其餘地方放著不動
+    A = np.absolute(F)
     R = np.real(np.fft.fftshift(np.fft.ifft(np.fft.ifftshift(F)))) #將F1做ifft得到新的real space
 
     diff_err = DiffErr.DiffErr(A,measured_A) #計算diffraction error
