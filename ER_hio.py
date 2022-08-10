26eb8ac0c0b36a7db30eb18a6c65c9c6a096761b
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-08-10 12:25:39 +0800

    adjust the proccess of support constraints

--- ER_hio.py
+++ ER_hio.py
@@ -1,7 +1,7 @@
 import numpy as np
 import DiffErr
 
-def ER_HIO(last_Rspace, support, measured_amplitude, patched_ROI):
+def ER_HIO(last_Rspace, support, alpha, measured_amplitude, patched_ROI):
     # last_Rspace為上一次跌代完後未加入support限制的Rspace
     # support為布林矩陣
     # measured_amplitude為實驗數據，已做完dopatch
@@ -10,17 +10,20 @@ def ER_HIO(last_Rspace, support, measured_amplitude, patched_ROI):
     measured_A = np.copy(measured_amplitude)
     measured_A[patched_ROI==False] = 0
 
-    last_Rspace[support==False] = 0 #將loose support外限制加入
+    R = np.zeros(np.shape(last_Rspace))
+    R[support==False] = alpha*last_Rspace[support==False] #將loose support外限制加入
     support_region = last_Rspace[support]
-    support_region[support_region<0] = 0
-    last_Rspace[support] = support_region #loose support內不能有電子密度<0
+    support_region[support_region<0] = alpha*support_region[support_region<0]
+    R[support] = support_region #loose support內不能有電子密度<0
 
-    F = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(last_Rspace))) #將上次的結果fft得到新的相位猜測
+    F = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R))) #將上次的結果fft得到新的相位猜測
     phase = np.angle(F) #取出F的相位資訊
     F[patched_ROI] = np.multiply(np.exp(1j*phase[patched_ROI]),measured_A[patched_ROI]) #將ROI區的相位套上measured amplitude，其餘地方放著不動
     A = np.absolute(F)
-    R = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F)))) #將F1做ifft得到新的real space
+    R_new= np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F)))) #將F1做ifft得到新的real space
 
-    diff_err = DiffErr.DiffErr(A,measured_A) #計算diffraction error
+    F_new = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R_new)))
+    A_new = np.absolute(F_new)
+    diff_err = DiffErr.DiffErr(A_new,measured_A) #計算diffraction error
 
-    return R,diff_err
+    return R_new,diff_err
