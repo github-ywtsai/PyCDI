65a9068235247b7d5972e31a7bb657360b52c9d9
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-08-11 18:33:34 +0800

    create alternative hio, change the timming to do support constraint

---
+++ alternative_hio.py
@@ -0,0 +1,33 @@
+import numpy as np
+import DiffErr
+
+def alter_HIO(last_Rspace, R, support, beta1, beta2, measured_amplitude, patched_ROI):
+    # last_Rspace為上一次跌代完後還未加入support限制的Rspace
+    # support為布林矩陣
+    # beta1為定值，用在loose support外
+    # beta2為定值，用在loose support內
+    # measured_amplitude為實驗數據，已做完dopatch
+    # patched_ROI為做完dopatch後的ROI
+    # last_Rspace = 上一次未做調整的Rspace；R = 上一次做完調整的Rspace，也就是上一次的R_
+    measured_A = np.copy(measured_amplitude)
+    measured_A[patched_ROI==False] = 0
+
+    R_ = np.zeros(np.shape(last_Rspace))
+    R_[support==False] = R[support==False]-beta1*last_Rspace[support==False] #將loose support外限制加入
+    support_region = last_Rspace[support]
+    last_support_region = last_Rspace[support]
+    support_region[support_region<0] = last_support_region[support_region<0] - beta2*support_region[support_region<0]
+    R_[support] = support_region #loose support內不能有電子密度<0
+
+    F_ = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R_))) #將上次的結果fft得到新的相位猜測
+    phase = np.angle(F_) #取出F的相位資訊
+    F_[patched_ROI] = np.multiply(np.exp(1j*phase[patched_ROI]),measured_A[patched_ROI]) #將ROI區的相位套上measured amplitude，其餘地方放著不動
+    A = np.absolute(F_)
+    R_new = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F_)))) #將F1做ifft得到新的real space
+
+    F_new = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R_new)))
+    A_new = np.absolute(F_new)
+    diff_err = DiffErr.DiffErr(A_new,measured_A) #計算diffraction error
+
+    return R_new,R_,diff_err
+    
--- hio_example.py
+++ hio_example.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import hio
 import Error_Reduction
 from PIL import Image
+import alternative_hio
 img = Image.open('IR.jpg')
 Object = img.convert('L')
 
@@ -33,16 +34,36 @@ R_int = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F_int))))
 plt.figure()
 plt.imshow(R_int[0:Object_size[0],0:Object_size[1]])
 
+#=========================== 做hio =============================================
+# for iter in range(200):
+#     R_hio,Err_hio = hio.HIO(R_int, support, 0.9, 0.9, amplitude, ROI)
+#     R_int = np.copy(R_hio)
+#     print(iter)
+
+# print(Err_hio)
+# plt.figure()
+# plt.imshow(R_hio)
+# plt.ioff()
+# plt.show()
+#==============================================================================
+
+#=========================== 做alternative_hio ================================
+R_const = np.zeros(np.shape(support)) #先隨便做一個初始值給第一次的support constrain用
 for iter in range(200):
-    R_hio,Err_hio = hio.HIO(R_int, support, 0.9, 0.9, amplitude, ROI)
-    R_int = np.copy(R_hio)
+    R_hio,R_,Err_hio = alternative_hio.alter_HIO(R_int, R_const, support, 0.9, 0.9, amplitude, ROI)
+    R_int = np.copy(R_hio) #這一次hio跑完未做support constrain的
+    R_const = np.copy(R_) #上一次的R_hio做完support constrain的
     print(iter)
+
 print(Err_hio)
 plt.figure()
-# plt.imshow(R_hio[s0:s0+Object_size[0], s1:s1+Object_size[1]])
 plt.imshow(R_hio)
 plt.ioff()
 plt.show()
+#===============================================================================
+
+
+
 
 # # # R_int = np.copy(R_hio)
 # n = 100
