80844a7e92aee9714200024b84e0530c05c9e3c2
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-08-19 14:01:48 +0800

    simplify the code

--- dynamic_support.py
+++ dynamic_support.py
@@ -1,22 +1,18 @@
+# X-ray image reconstruction from a diffraction pattern alone
+# page2 paragraph2
 import numpy as np
 import scipy.signal
 
 
-def dynamic_supp(last_Rspace, autocorr_support, threshold, std_deviation):
-    size = np.shape(last_Rspace)
+def dynamic_supp(rho_p, threshold, std_deviation):
+    size = np.shape(rho_p)
     x, y = np.meshgrid(np.linspace(-1,1,size[1]), np.linspace(-1,1,size[0]))
     d = np.sqrt(x*x+y*y)
     sigma, mu = std_deviation, 0.0
     Gaussion = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
 
-    multi = scipy.signal.fftconvolve(last_Rspace,Gaussion,mode = 'same')
+    multi = scipy.signal.fftconvolve(rho_p,Gaussion,mode = 'same')
     adj_multi = np.full(np.shape(multi), False)
     adj_multi[multi > np.max(multi)*threshold] = True
-    # adj_multi[autocorr_support==0] = False
 
-    # 以下在把切過的support再放大一點 避免切太深
-    # b = np.ones((2,2))
-    # support_conv = scipy.signal.fftconvolve(adj_multi,b,mode = 'same') 
-    # adj_support = np.full(np.shape(adj_multi), False)
-    # adj_support[support_conv>0] = True
     return adj_multi
--- dynamic_support_example.py
+++ dynamic_support_example.py
@@ -7,73 +7,74 @@ import alternative_hio
 import dynamic_support
 import scipy.signal
 
+#做一個IR sample
 img = Image.open('IR.jpg')
 Object = img.convert('L')
-# plt.figure()
-# plt.imshow(Object)
-# plt.title("Object")
 
 Object_size = np.shape(Object)
 support = np.full((5*Object_size[0], 5*Object_size[1]), False)
 support_size = np.shape(support)
 s0 = round(support_size[0]/2)-round(Object_size[0]/2)
 s1 = round(support_size[1]/2)-round(Object_size[1]/2)
-# support[s0:s0+Object_size[0], s1:s1+Object_size[1]] = True
+support[s0:s0+Object_size[0], s1:s1+Object_size[1]] = True
 
 R = np.zeros(np.shape(support))
 R[s0:s0+Object_size[0], s1:s1+Object_size[1]] = Object
 plt.figure()
 plt.imshow(R)
 plt.title("Object")
-amplitude = np.abs(np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R)))) #此代表量測到的amplitude
+
+amplitude = np.abs(np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(R))))
 intensity = amplitude**2
 ROI = np.full((5*Object_size[0], 5*Object_size[1]), True)
-random_Phase = np.random.uniform((-1)*np.pi,np.pi,np.shape(amplitude)) #做一組隨機相位
-F_int = np.multiply(amplitude, np.exp(1j*random_Phase))
-R_int = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(F_int))))
-plt.figure()
-plt.imshow(R_int)
-plt.title("R_int")
 
+#做初始參數
+random_Phase = np.random.uniform((-1)*np.pi,np.pi,np.shape(amplitude)) #做一組隨機相位
+G_init = amplitude* np.exp(1j*random_Phase)
+rho_p_init = np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(G_init))))
+plt.figure()
+plt.imshow(rho_p_init)
+plt.title("rho_p_init")
+
+#做auto correlation support
 adj_inten = np.copy(intensity)
 adj_inten[intensity<intensity*4/100] = 0
-autoCorr_R_int = np.abs(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(adj_inten))))
-autoCorr_R_int = autoCorr_R_int.astype(int)
-autocorr_support = np.full(np.shape(autoCorr_R_int), False)
-autocorr_support[autoCorr_R_int > 0] = True
-# autocorr_support[autoCorr_R_int >= np.max(autoCorr_R_int)*4/100] = True #得到一個初始support
+autoCorr_rho_p_init = np.abs(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(adj_inten))))
+autoCorr_rho_p_init = autoCorr_rho_p_init.astype(int)
+autocorr_support = np.full(np.shape(autoCorr_rho_p_init), False)
+autocorr_support[autoCorr_rho_p_init > 0] = True
 
 plt.figure()
 plt.imshow(autocorr_support)
 plt.title("autocorr_support")
 
-R_const = np.zeros(np.shape(autocorr_support)) #先隨便做一個初始值給第一次的support constrain用
+last_rho_init = np.zeros(np.shape(autocorr_support)) #先隨便做一個初始值給第一次的support constrain用
 for iter in range(20):
-    R_hio,R_,Err_hio = alternative_hio.alter_HIO(R_int, R_const, autocorr_support, 0.9, 0.9, amplitude, ROI)
-    R_int = np.copy(R_hio) #這一次hio跑完未做support constrain的
-    R_const = np.copy(R_) #上一次的R_hio做完support constrain的
+    new_rho_p,new_rho,diff_err = alternative_hio.alter_HIO(rho_p_init, last_rho_init, support, 0.9, amplitude, ROI)
+    rho_p_init = np.copy(new_rho_p) #這一次hio跑完未做support constrain的
+    last_rho_init = np.copy(new_rho) #上一次的R_hio做完support constrain的
     print(iter)
 plt.figure()
-plt.imshow(R_hio)
-plt.title("R_hio")
+plt.imshow(new_rho_p)
+plt.title("new_rho_p")
 
-std_deviation = 0.02
+std_deviation = 0.03
 for iter2 in range(20):
     print(iter2)
-    new_support = dynamic_support.dynamic_supp(R_hio, autocorr_support, 20/100, std_deviation)
+    new_support = dynamic_support.dynamic_supp(new_rho_p, 20/100, std_deviation)
     plt.figure()
     plt.imshow(new_support)
     for iter in range(20):
-        R_hio,R_,Err_hio = alternative_hio.alter_HIO(R_int, R_const, new_support, 0.9, 0, amplitude, ROI)
-        R_int = np.copy(R_hio) #這一次hio跑完未做support constrain的
-        R_const = np.copy(R_) #上一次的R_hio做完support constrain的
+        new_rho_p,new_rho,diff_err = alternative_hio.alter_HIO(rho_p_init, last_rho_init, new_support, 0.9, amplitude, ROI)
+        rho_p_init = np.copy(new_rho_p) #這一次hio跑完未做support constrain的
+        last_rho_init = np.copy(new_rho) #上一次的R_hio做完support constrain的
 
     std_deviation = std_deviation-0.0005
 
-print(Err_hio)
+print(diff_err)
 plt.figure()
-plt.imshow(R_hio)
-plt.title("R_hio")
+plt.imshow(new_rho_p)
+plt.title("new_rho_p")
 plt.figure()
 plt.imshow(new_support)
 plt.title("new_support")
