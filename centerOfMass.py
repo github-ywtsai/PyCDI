a3036476ab747aa2341ed37ef9e8f3c1cc6f48d8
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-09-21 12:10:00 +0800

    更改變數名稱

--- centerOfMass.py
+++ centerOfMass.py
@@ -1,6 +1,6 @@
 import numpy as np
 def CM(Array2D):
-    size = np.shape(Array)
+    size = np.shape(Array2D)
     row_index = np.zeros(size)
     col_index = np.zeros(size)
     for n in range(1,size[0]+1):
@@ -8,8 +8,8 @@ def CM(Array2D):
     for m in range(1,size[1]+1):
         col_index[:,m-1] = m
 
-    row_center = round(np.sum(Array*row_index)/np.sum(Array))-1
-    col_center = round(np.sum(Array*col_index)/np.sum(Array))-1
+    row_center = round(np.sum(Array2D*row_index)/np.sum(Array2D))-1
+    col_center = round(np.sum(Array2D*col_index)/np.sum(Array2D))-1
 
 
     return row_center,col_center
