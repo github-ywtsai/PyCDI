75e88bff566a8198bdc9c177ca07ffdd30f81cf2
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-09-25 11:22:09 +0800

    發現原本的dynamic support內部是用複數運算，所以改為abs再運算

--- dynamic_support.py
+++ dynamic_support.py
@@ -6,14 +6,15 @@ from cupyx.scipy.signal import fftconvolve
 import cupy as cp
 
 def dynamic_supp(rho_p, threshold, std_deviation):
-    size = np.shape(rho_p)
+    R = np.abs(rho_p)
+    size = np.shape(R)
     x, y = np.meshgrid(np.linspace(-1,1,size[1]), np.linspace(-1,1,size[0]))
     d = np.sqrt(x*x+y*y)
     sigma, mu = std_deviation, 0.0
     Gaussion = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
     Gaussion = cp.asarray(Gaussion)
 
-    multi = fftconvolve(rho_p,Gaussion,mode = 'same')
+    multi = fftconvolve(R,Gaussion,mode = 'same')
     adj_multi = cp.full(np.shape(multi), False)
     adj_multi[multi > cp.amax(multi)*threshold] = True
 
