d41ce8ffd473e03a465b3c86124cd53171b043b0
yeh.ch <yeh.ch@nsrrc.org.tw>
2022-10-17 03:07:10 +0800

    fix bug

--- FSC_patch.py
+++ FSC_patch.py
@@ -7,10 +7,10 @@ def FSC_patch(complex1,complex2):
     R2 = np.abs(complex2)
     Rcenter = [round((np.shape(R1)[0]+1)/2)-1,round((np.shape(R1)[1]+1)/2)-1]
     R1_CM = CM.CM(R1)
-    complex1 = np.roll(complex1,Rcenter[0]-R1_CM[0],axis=0)
-    complex1 = np.roll(complex1,Rcenter[1]-R1_CM[1],axis=1) #將R1質心移至array中心
-    corr1 = scipy.signal.correlate(R1,R2)
-    corr2 = scipy.signal.correlate(R1,np.flip(R2)) #計算兩者的互相關
+    A = np.roll(complex1,Rcenter[0]-R1_CM[0],axis=0)
+    A = np.roll(A,Rcenter[1]-R1_CM[1],axis=1) #將R1質心移至array中心
+    corr1 = scipy.signal.correlate(np.abs(A),R2)
+    corr2 = scipy.signal.correlate(np.abs(A),np.flip(R2)) #計算兩者的互相關
     if np.max(corr2)>np.max(corr1): #檢查是否有flip
         complex2 = np.flip(complex2)
         corr = corr2
@@ -18,7 +18,7 @@ def FSC_patch(complex1,complex2):
         corr = corr1
     corrCenter = [round((np.shape(corr)[0]+1)/2)-1,round((np.shape(corr)[1]+1)/2)-1]
     corrMax = np.where(corr==np.max(corr))
-    complex2 = np.roll(complex2,corrMax[0][0]-corrCenter[0],axis=0)
-    complex2 = np.roll(complex2,corrMax[1][0]-corrCenter[1],axis=1) #將R2移至與R1最重合的位置
+    B = np.roll(complex2,corrMax[0][0]-corrCenter[0],axis=0)
+    B = np.roll(B,corrMax[1][0]-corrCenter[1],axis=1) #將R2移至與R1最重合的位置
 
-    return complex1,complex2
+    return A,B
